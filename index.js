0bdd9bcd495a37f9c3b82b6dedea7c366f3bf494
Aleks <aleksmllr@gmail.com>
2023-04-06 19:15:49 -0700

    Configuring for Render...

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-
 const cors = require("cors");
 
-app.use(cors());
-
-app.use(express.json());
+const Note = require("./models/note");
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -15,8 +13,13 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-app.use(requestLogger);
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
+app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
 app.use(express.static("build"));
 
 let notes = [
@@ -37,30 +40,15 @@ let notes = [
   },
 ];
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/notes", (request, response) => {
-  response.json(notes);
-});
-
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
-});
-
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
+app.get("/api/notes", (req, res) => {
+  Note.find({}).then((notes) => {
+    //console.log("notes: ", notes);
+    res.json(notes);
+  });
 });
 
 const generateId = () => {
@@ -80,6 +68,7 @@ app.post("/api/notes", (request, response) => {
   const note = {
     content: body.content,
     important: body.important || false,
+    date: new Date(),
     id: generateId(),
   };
 
@@ -88,7 +77,29 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-const PORT = process.env.PORT || 3001;
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+
+  response.json(note);
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
