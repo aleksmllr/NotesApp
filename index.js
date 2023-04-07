4bc3cfb53bda5f9fee1a1a0652a506665437c782
Aleks <aleksmllr@gmail.com>
2023-04-06 19:40:53 -0700

    Rebuilt front end and make changes to configure frontend with backend/db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 
 const Note = require("./models/note");
 
@@ -22,72 +22,35 @@ app.use(express.json());
 app.use(requestLogger);
 app.use(express.static("build"));
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
+let notes = [];
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
-
-app.get("/api/notes", (req, res) => {
+app.get("/api/notes", (request, response) => {
   Note.find({}).then((notes) => {
-    //console.log("notes: ", notes);
-    res.json(notes);
+    response.json(notes);
   });
 });
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
-
-  response.json(note);
+  });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -103,5 +66,3 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-//
--- mongo.js
+++ mongo.js
@@ -7,27 +7,31 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://aleksmllr:${password}@cluster0.frytfkc.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
   content: String,
+  date: Date,
   important: Boolean,
 });
 
 const Note = mongoose.model("Note", noteSchema);
 
-// const note = new Note({
-//   content: "HTML is Easy",
-//   important: true,
-// });
+const note = new Note({
+  content: "Mongoose makes things easy",
+  date: new Date(),
+  important: true,
+});
 
-// note.save().then((result) => {
-//   console.log("note saved!");
-//   mongoose.connection.close();
-// });
+/*
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
 
 Note.find({}).then((result) => {
   result.forEach((note) => {
